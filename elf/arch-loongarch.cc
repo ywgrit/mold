93774a1095739ce585575522c30350c15c1a55d5
Xin Wang <wangxin03@loongson.cn>
2024-08-14 11:32:08 +0800

    Add the reloc types of tls desc when scan relocation

--- elf/arch-loongarch.cc
+++ elf/arch-loongarch.cc
@@ -784,6 +784,11 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_LARCH_ADD_ULEB128:
     case R_LARCH_SUB_ULEB128:
     case R_LARCH_TLS_LE_ADD_R:
+    case R_LARCH_TLS_DESC_PC_LO12:
+    case R_LARCH_TLS_DESC64_PC_HI12:
+    case R_LARCH_TLS_DESC64_PC_LO20:
+    case R_LARCH_TLS_DESC_LD:
+    case R_LARCH_TLS_DESC_CALL:
       break;
     default:
       Error(ctx) << *this << ": unknown relocation: " << rel;
